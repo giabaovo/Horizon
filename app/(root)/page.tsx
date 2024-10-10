a3fc99b4466fae774e67fcc0b8994670333705cf
Bao Vo <giabaovo123456@gmail.com>
2024-10-10 13:39:44 +0700

    Implement header box UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 function Home() {
+    const loggedIn = {firstName: "Bao Vo"}
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        subtext="ABCB"
+                        user={loggedIn?.firstName || "Guest"}
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1234.45}
+                    />
+                </header>
+            </div>
+        </section>
     );
 }
 
